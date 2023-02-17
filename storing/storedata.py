a70b85f7113f3e6d18215e733db7223e75dfa3e2
DON J VELLATHOTTAM <90134636+vellathottam@users.noreply.github.com>
2023-02-17 12:32:52 +0530

    Add files via upload

---
+++ storing/storedata.py
@@ -0,0 +1,95 @@
+import cv2
+import face_recognition
+import datetime
+import csv
+
+# Load the known faces and names from a CSV file
+known_faces = []
+known_names = []
+
+# Open the file containing the names
+with open(r'C:\Users\donve\Desktop\project\mode1\names.txt', 'r') as file:
+    # Read the lines from the file
+    lines = file.readlines()
+
+# Create an empty list to store the names
+names = []
+
+# Loop over the lines in the file
+for line in lines:
+    # Remove the newline character from the end of the line
+    line = line.strip()
+    # Add the name to the list
+    names.append(line)
+
+# Load the images of known individuals
+for name in names:
+    image = face_recognition.load_image_file(f"C:\\Users\\donve\\Desktop\\project\\mode1\\{name}.jpg")
+    face_encoding = face_recognition.face_encodings(image)[0]
+    known_faces.append(face_encoding)
+    known_names.append(name)
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+frame_number = 0
+
+# Initialize the video capture object
+video_capture = cv2.VideoCapture(0)
+
+# Loop over frames from the video file stream
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
+
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known faces
+        matches = face_recognition.compare_faces(known_faces, face_encoding)
+
+        # If a match was found in known_faces, just use the first one.
+        # If a match was not found, set the name to "Unknown"
+        name = "Unknown"
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_names[first_match_index]
+
+        # Add the name to the list of names
+        face_names.append(name)
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Draw a rectangle around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Log the recognition data in a CSV file
+        with open('recognition_log.csv', 'a') as file:
+            writer = csv.writer(file)
+            writer.writerow([datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), name, top, right, bottom, left])
+
+    # Increment the frame counter
+    frame_number += 1
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close the display window
+video_capture.release()
+cv2.destroyAllWindows()
